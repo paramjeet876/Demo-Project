42aa170b87460b5cae51f850e7f64bd8ea140e23
paramjeet101 <parammahato8454@gmail.com>
2025-03-08 12:53:18 +0530

    fixes

--- core/settings.py
+++ core/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://demo-project-d5j4.onrender.com'
+]
